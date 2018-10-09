acf74fc7b3f19efe5d242d2ab6cf203b5b5e97c1
maymayan <mmaymayan@gmail.com>
2018-10-09 18:56:51 +0300

    Finished heroku deployment configuration

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,6 +17,7 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class App 
 {
      public static void main(String[] args) {
+         System.out.println("Hello World");
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> "Hello, World");
