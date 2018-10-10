8c5a3a62116d2ae1692aeed0756bdbb8f380e8a5
maymayan <mmaymayan@gmail.com>
2018-10-10 03:11:14 +0300

    Last modify before submit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ﻿Bu proje 2 adet integer dizisi ve 2 adet integer'ı alır ve bu 2 integer'ın toplamından elde edilen sayıyı dizilerin içinde
 arar. Eğer 2 dizide de aynı sayıda bulursa doğru sonucunu döner bulamazsa yanlış sonucunu döner.
 [![Build Status](https://travis-ci.org/maymayan/myDemoApp.svg?branch=master)](https://travis-ci.org/maymayan/myDemoApp)
+
+
 Heroku demo: https://floating-crag-60783.herokuapp.com
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -24,8 +24,12 @@ public class AppTest extends TestCase{
       ArrayList<Integer> arr2 = new ArrayList<>();
       assertFalse(new App().search(arr1, arr2, 1, 2));
     }
-
-    
+    public void testTrue(){
+      
+      ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(5, 6, 7, 7));
+      ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(7, 7, 3, 4));
+      assertTrue(new App().search(arr1, arr2, 7, 0));
+    }
     public void testNullInteger(){
       ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
       ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1, 2, 3, 3));
